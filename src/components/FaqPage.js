0e5314f1def98103090ea776de5ea1d45cfbfadc
jonathanchiu135 <jonathanchiu135@gmail.com>
2022-08-05 01:01:23 -0700

    added back some homepage styles (we should discuss what background we want); cleaned up some basic faq and schedule styles

--- src/components/FaqPage.js
+++ src/components/FaqPage.js
@@ -4,50 +4,49 @@ import Faq from "react-faq-component";
 
 const FaqPage = () => { 
     const data = {
-    title: "FAQ",
-    rows: [
-        {
-            title: "Q: What is HackCMU?",
-            content: `100000% best hackathon at CMU, no cap my homies!!!!! This is the absolute truth and only the truth!!`,
-        },
-        {
-            title: "Q: Where is HackCMU?",
-            content:
-                `Probably in Gates, but idk! No details at this point yet! We will probably figure this out at some point broskis!`,
-        },
-        {
-            title: "Q: Why is HackCMU?",
-            content: `We want free stuff! You want free stuff! It all works out! Less gooo!!!`,
-        },
-        {
-            title: "Q: How much does this cost?",
-            content: `$0! We'll provide meals, snacks, drinks, and lots of swag, thanks to the support of our wonderful sponsors.`,
-        },
-        {
-            title: "Q: I’m not a “hacker”, can I still participate?",
-            content: `Students of all skill levels are encouraged to attend, even if you’ve never written a line of code! Creative individuals of all kinds are welcome to apply — such as designers, ECE majors, statistics, etc. The only requirement is your attitude to learn!`,
-        },
-        {
-            title: "Q: Are there teams? Do I need one to sign up!",
-            content: `Working as a team is highly encouraged, but not required. We'll be facilitating team-forming the day-of, so don't be discouraged if you don't have a team!`,
-        },
-        {
-            title: "Q: How big can a team be?!",
-            content: `Up to 4 people`,
-        },
-        {
-            title: "Q: What are the rules? Can I work on my project ahead of time?",            
-            content: `All work must be original, and you are not permitted to start building or designing your project until the event. Brainstorming ideas and forming teams beforehand is fine, though.`,
-        },
-        {
-            title: "Q: Who runs this event?",            
-            content: `You can learn more about us over here.`,
-        },
-        {
-            title: "Q: Any other questions, comments, or concerns?",            
-            content: `Email us at acm-exec[at]cs.cmu.edu.`,
-        },
-    ]
+        rows: [
+            {
+                title: "Q: What is HackCMU?",
+                content: `100000% best hackathon at CMU, no cap my homies!!!!! This is the absolute truth and only the truth!!`,
+            },
+            {
+                title: "Q: Where is HackCMU?",
+                content:
+                    `Probably in Gates, but idk! No details at this point yet! We will probably figure this out at some point broskis!`,
+            },
+            {
+                title: "Q: Why is HackCMU?",
+                content: `We want free stuff! You want free stuff! It all works out! Less gooo!!!`,
+            },
+            {
+                title: "Q: How much does this cost?",
+                content: `$0! We'll provide meals, snacks, drinks, and lots of swag, thanks to the support of our wonderful sponsors.`,
+            },
+            {
+                title: "Q: I’m not a “hacker”, can I still participate?",
+                content: `Students of all skill levels are encouraged to attend, even if you’ve never written a line of code! Creative individuals of all kinds are welcome to apply — such as designers, ECE majors, statistics, etc. The only requirement is your attitude to learn!`,
+            },
+            {
+                title: "Q: Are there teams? Do I need one to sign up!",
+                content: `Working as a team is highly encouraged, but not required. We'll be facilitating team-forming the day-of, so don't be discouraged if you don't have a team!`,
+            },
+            {
+                title: "Q: How big can a team be?!",
+                content: `Up to 4 people`,
+            },
+            {
+                title: "Q: What are the rules? Can I work on my project ahead of time?",            
+                content: `All work must be original, and you are not permitted to start building or designing your project until the event. Brainstorming ideas and forming teams beforehand is fine, though.`,
+            },
+            {
+                title: "Q: Who runs this event?",            
+                content: `You can learn more about us over here.`,
+            },
+            {
+                title: "Q: Any other questions, comments, or concerns?",            
+                content: `Email us at acm-exec[at]cs.cmu.edu.`,
+            },
+        ]
     }
     
     const config = {
@@ -56,12 +55,15 @@ const FaqPage = () => {
         // tabFocus: true
     };
     return (
-        <div>
+        <div className="faq-page">
+            <div className="faq-header"></div> {/* used to line up the scroll correctly */}
+            <div className='faq-title'>FAQ</div>
             <Faq
                 data={data}
                 styles={ '../styles/Faq.css'}
                 config={config}
             />
+            <div className="faq-footer"></div> {/* used to line up the scroll correctly */}
         </div>
     );
 
--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -7,6 +7,7 @@ const Prizes = () => {
                 <div className='prizes-header'></div>
                 <div className='prizes-title'>Prizes</div>
                 <div>Probably some nice free stuff - we should put some pictures up</div>
+                <div>I think we want to set up some kind of "card" component to list out prizes</div>
             </div>
         </div>
     );

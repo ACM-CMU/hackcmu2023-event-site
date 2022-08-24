5efee04eb381efcdbf53ac36e9b20f424e316ac6
jonathanchiu135 <jonathanchiu135@gmail.com>
2022-08-23 23:50:36 -0400

    revamp prizes page (PLZ TEST)

--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -1,218 +1,84 @@
 import '../styles/Prizes.css'
-import { Card, CardContent, Typography, CardMedia, Grid } from '@mui/material'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image'
 
 const Prizes = () => { 
+    const images = [
+        "../../imgs/mechkey-2.jpg", 
+        "../../imgs/lego.jpg", 
+        "../../imgs/echo.jpg", 
+        "../../imgs/charger-2.jpg", 
+        "../../imgs/pinkblueoctopus.jpg", 
+        "../../imgs/grayblueoctopus.jpg", 
+        "../../imgs/turtle.jpg", 
+        "../../imgs/pinkbluenarwhal.jpg", 
+        "../../imgs/blackwhitenarwhal.jpg"
+    ];
     return (  
         <div className="prizes-page">
             <div className="prizes-content">
                 <div className='prizes-header'></div>
                 <div className='prizes-title'>Prizes</div>
-                <div className='prizes-caption'>
-                    <div className='prizes-para'>Below are the prizes in this year's prize pool! Prizes will be awarded to:</div>
-                    <ul className='prizes-list'>
-                        <li>Top three best hack winners</li>
-                        <li>Best "Gamification" hack (this year's theme)</li>
-                        <li>A whopping <b>25</b> individual raffle winners</li>
-                    </ul>  
+                <div className='prizes-img-and-caption'>
+                    <div className='prizes-caption'>
+                        <div className='prizes-caption-header'></div>
+                        <div className='prizes-para'>Here are the prizes in this year's prize pool! Prizes will be awarded to:</div>
+                        <br></br>
+                        <ul className='prizes-list'>
+                            <li>Top three best hack winners</li>
+                            <li>Best "Gamification" hack (this year's theme)</li>
+                            <li>A whopping <b>25</b> individual raffle winners</li>
+                        </ul>  
+                    </div>
+                    <div className='prizes-caption-img-sep'></div>
+                    <Slide>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[0]})` }}>
+                                <span>Mechanical Keyboard</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[1]})` }}>
+                                <span>LEGO Infinity Gauntlet</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+                                <span>Echo Dot (4th Gen)</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[3]})` }}>
+                                <span>Portable Charger</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[4]})` }}>
+                                <span>Reversible Octopus Plush - Pink/Blue (raffle)</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[5]})` }}>
+                                <span>Reversible Octopus Plush - Gray/Blue (raffle)</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[6]})` }}>
+                                <span> Reversible Turtle Plush (raffle)</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[7]})` }}>
+                                <span>Reversible Narwhal Plush - Pink/Blue (raffle)</span>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect">
+                            <div style={{ 'backgroundImage': `url(${images[8]})` }}>
+                                <span>Reversible Narwhal Plush - Black/White (raffle)</span>
+                            </div>
+                        </div>
+                    </Slide>
                 </div>
-                <br></br>
-                <Grid
-                    container
-                    flexDirection="row"
-                    justifyContent="center"
-                    alignContent="center"
-                    spacing={10}
-                    >
-                    <Grid item>
-                        <Card variant="outlined" sx={{width: "40vh", height: "40vh", margin:"auto"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/mechkey.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Mechanical Keyboard
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Winner Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/lego.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    LEGO Infinity Gauntlet
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Winner Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/echo.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Echo Dot 4th Generation
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Winner Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-                <br></br>
-                <Grid
-                    container
-                    flexDirection="row"
-                    justifyContent="center"
-                    alignContent="center"
-                    spacing={10}
-                    >
-                    <Grid item>
-                        <Card variant="outlined" sx={{width: "40vh", height: "40vh", margin:"auto"}}>
-                            <CardMedia
-                                component="img"
-                                height="280"
-                                image="../../imgs/charger.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Portable Charger
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Winner Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/pinkblueoctopus.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Reversible Octopus Plush (Pink and Blue)
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Raffle Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/grayblueoctopus.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Reversible Octopus Plush (Gray and Blue)
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Raffle Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-                <br></br>
-                <Grid
-                    container
-                    flexDirection="row"
-                    justifyContent="center"
-                    alignContent="center"
-                    spacing={10}
-                    >
-                    <Grid item>
-                        <Card variant="outlined" sx={{width: "40vh", height: "40vh", margin:"auto"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/turtle.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Reversible Turtle Plush
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Raffle Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/pinkbluenarwhal.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Reversible Narwhal Plush (Pink and Blue)
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Raffle Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                    <Card variant="outlined" sx={{width: "40vh", height: "40vh"}}>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="../../imgs/blackwhitenarwhal.jpg"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="div">
-                                    Reversible Narwhal Plush, Black and White
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    (Raffle Prize)
-                                </Typography>
-                                <Typography variant="body2">
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
                 <br></br>
                 <br></br>
             </div>

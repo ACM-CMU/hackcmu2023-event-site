e5353f0f5a5a4e08fedae86874bcbe1ed359afcf
jonathanchiu135 <jonathanchiu135@gmail.com>
2022-08-24 23:27:31 -0400

    updated sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -5,15 +5,18 @@ const Sponsors = () => {
         <div className="sponsors-page">
             <div className="sponsors-header"></div> {/* used to line up the scroll correctly */}
             <div className='sponsors-title'>sponsors</div>
+            <div className='sponsors-level'>PLATINUM</div>
+            <div className='sponsors sponsors-platinum'>   
+                <a href="https://www.citadel.com">
+                    <img src="../../citadel-logo.png" alt="sponsors"/>
+                </a>
+            </div>
             <div className='sponsors-level'>GOLD</div>
             <div className='sponsors sponsors-gold'>
                 <a href="https://www.microsoft.com/en-us/">
                     <img src="../../microsoft-logo2.png" alt="sponsors"/>
                     </a>
-                <a href="https://www.citadel.com">
-                    <img src="../../citadel-logo.png" alt="sponsors"/>
-                    </a>
-                <a href="https://www.hudsonrivertrading.com/">
+                <a className="hrt-logo" href="https://www.hudsonrivertrading.com/">
                     <img src="../../hrt-logo-clipart.png" alt="sponsors"/>
                     </a>
                 <a className="rounded-sponsor-img" href="https://www.bloomberg.com/">
@@ -25,12 +28,18 @@ const Sponsors = () => {
                 <a href="https://www.aptiv.com">
                     <img src="../../aptiv-cropped.png" alt="sponsors"/>
                     </a>
+                <a href="https://about.facebook.com/">
+                    <img src="../../meta-logo.png" alt="sponsors"/>
+                    </a>
             </div>
             <br></br>
             <div className='sponsors-level'>SILVER</div>
             <div className='sponsors sponsors-silver'>
                 <a href="https://www.oldmissioncapital.com/">
                     <img src="../../omc-logo.png" alt="sponsors"/>
+                    </a>
+                <a href="https://www.sandia.gov/">
+                    <img src="../../sandia-logo.png" alt="sponsors"/>
                     </a>
             </div>
             <br></br>

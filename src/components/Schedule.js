766af76d3629bfcdfa23391e454cac39ac9e20d3
Your Name <s026134@students.lmsd.org>
2023-08-24 02:10:19 -0400

    schedule updates

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -6,11 +6,11 @@ const fridaySchedule = () => {
     <div>
       <div class="schedule-event main-event">
         <div class="col">
-          <p>5:30 - 6:00 PM</p>
+          <p>5:00 - 5:45 PM</p>
         </div>
         <div class="col">
           <p>Check-in</p>
-          <p class="details">Outside Rashid Auditorium</p>
+          <p class="details">Tepper 1101</p>
         </div>
       </div>
       <div class="schedule-event main-event">
@@ -19,44 +19,39 @@ const fridaySchedule = () => {
         </div>
         <div class="col">
           <p>Opening Ceremony</p>
-          <p class="details">Rashid Auditorium</p>
+          <p class="details">Tepper 1101</p>
+          <p class="details">Join us for the opening ceremony of HackCMU 2023!</p>
         </div>
       </div>
       <div class="schedule-event food">
         <div class="col">
-          <p>6:30 - 7:30 PM</p>
+          <p>7:00 - 9:00 PM</p>
         </div>
         <div class="col">
-          <p>Dinner - Bull River Tacos (Taco Truck)</p>
-          <p class="details">Outside NSH</p>
-        </div>
-      </div>
-      <div class="schedule-event workshops">
-        <div class="col">
-          <p>8:00 - 8:30 PM</p>
-        </div>
-        <div class="col">
-          <p>Workshop 1</p>
-          <p class="details">Rashid Auditorium (Beginner)</p>
-          <p class="details">ASA Conference Room (Advanced)</p>
+          <p>Dinner & Sponsor Fair</p>
+          <p class="details">Tepper 1101</p>
+          <p class="details">Meet our amazing event sponsors!</p>
         </div>
       </div>
       <div class="schedule-event workshops">
         <div class="col">
-          <p>8:45 - 9:30 PM</p>
+          <p>9:30 - 10:00 PM</p>
         </div>
         <div class="col">
-          <p>Workshop 2</p>
-          <p class="details">Rashid Auditorium (Beginner)</p>
-          <p class="details">ASA Conference Room (Advanced)</p>
+          <p>Workshop A / Team Match B</p>
+          <p class="details">GHC 2401</p>
+          <p>Workshop B</p>
+          <p class="details">CIC 1201</p>
+          <p>Workshop C</p>
+          <p class="details">NSH 1301</p>
         </div>
       </div>
       <div class="schedule-event food">
         <div class="col">
-          <p>11:00 PM</p>
+          <p>12:00 - 1:00 PM</p>
         </div>
         <div class="col">
-          <p>Midnight Snacks - Waffalonia</p>
+          <p>Midnight Snack</p>
           <p class="details">Gates La Prima</p>
         </div>
       </div>
@@ -69,65 +64,40 @@ const saturdaySchedule = () => {
     <div>
       <div class="schedule-event food">
         <div class="col">
-          <p>9:30 - 10:00 AM</p>
+          <p>8:00 - 10:00 AM</p>
         </div>
         <div class="col">
-          <p>Breakfast - Bruegger's Bagels</p>
-          <p class="details">Reddy Conference Room</p>
-        </div>
-      </div>
-      <div class="schedule-event food">
-        <div class="col">
-          <p>12:00 - 1:00 PM</p>
-        </div>
-        <div class="col">
-          <p>Lunch - Mineo's Pizza</p>
-          <p class="details">Reddy Conference Room</p>
+          <p>Breakfast</p>
+          <p class="details">Tepper 1101</p>
+          <p class="details">Get breakfast after a long night of hacking!</p>
         </div>
       </div>
       <div class="schedule-event main-event">
         <div class="col">
-          <p>2:00 - 3:00 PM</p>
+          <p>11:00 - 11:30 PM</p>
         </div>
         <div class="col">
-          <p>Sponsor Fair & BOBA</p>
-          <p class="details">Simmons B</p>
-        </div>
-      </div>
-      <div class="schedule-event main-event">
-        <div class="col">
-          <p>3:30 - 5:45 PM</p>
-        </div>
-        <div class="col">
-          <p>Expo Setup</p>
-          <p class="details">Simmons A & B</p>
+          <p>Project Submission Deadline</p>
+          <p class="details">Devpost</p>
         </div>
       </div>
       <div class="schedule-event main-event">
         <div class="col">
-          <p>6:00 - 7:30 PM</p>
+          <p>12:00 - 1:30 PM</p>
         </div>
         <div class="col">
-          <p>Judging</p>
-          <p class="details">Simmons A & B</p>
-        </div>
-      </div>
-      <div class="schedule-event food">
-        <div class="col">
-          <p>7:30 - 8:00 PM</p>
-        </div>
-        <div class="col">
-          <p>Dinner - 98K Hamburger</p>
-          <p class="details">Reddy Conference Room</p>
+          <p>Lunch & Project Expo</p>
+          <p class="details">Tepper 1101</p>
         </div>
       </div>
       <div class="schedule-event main-event">
         <div class="col">
-          <p>8:00 PM</p>
+          <p>2:00 - 2:30 PM</p>
         </div>
         <div class="col">
           <p>Closing Ceremony</p>
-          <p class="details">Simmons A & B</p>
+          <p class="details">Tepper 1101</p>
+          <p class="details">We hope you continue to reach for the stars after our event :)</p>
         </div>
       </div>
     </div>
@@ -153,7 +123,7 @@ const Schedule = () => {
     <div className="schedule-page">
       <div className="schedule-content">
         <div className="schedule-header"></div>
-        <div className="schedule-title">schedule</div>
+        <div className="schedule-title">Schedule</div>
         <div class="legend">
           <div class="legend-event">
             <div class="legend-box food"></div>
@@ -170,14 +140,14 @@ const Schedule = () => {
         </div>
         <div class="schedule-days">
           <button class="schedule-day-button" id="friday" onClick={showFriday}>
-            <span class="schedule-header-word">FRIDAY 9/16</span>
+            <span class="schedule-header-word">FRIDAY 9/15</span>
           </button>
           <button
             class="schedule-day-button"
             id="saturday"
             onClick={showSaturday}
           >
-            <span class="schedule-header-word">SATURDAY 9/17</span>
+            <span class="schedule-header-word">SATURDAY 9/16</span>
           </button>
         </div>
         <div class="schedule" id="schedule">

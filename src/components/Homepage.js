0e03e2290497704fcd928227c6a75f957a1d5702
Neeha Kurelli <62197412+nkurelli@users.noreply.github.com>
2023-08-30 16:46:44 -0400

    changed deployment

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,19 +5,6 @@ const Homepage = () => {
         <div className="home-page">
             
             <div className="home-page-content">
-                {/* <div className="home-page-center-content">
-                    <div className="home-page-logo">HACKCMU 2022</div>
-                    <div className="home-page-caption">PRESENTED BY ACM@CMU</div>
-                </div> */}
-                {/* <div className="home-page-circle-container">
-                    <div className="home-page-circle" id="home-page-circle-1">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div> */}
-                
                 <div class="section" id="home">
                 <div id="star-container">
                     <div id='stars'></div>

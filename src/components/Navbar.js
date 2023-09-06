8d583623f1c529a5c15bec8521ec6330274f0d03
aaravbajaj012 <97072650+aaravbajaj012@users.noreply.github.com>
2023-09-06 11:36:28 -0400

    prize titles and mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
-import '../styles/Navbar.css'
-import React, { useState } from 'react';
-import { Link } from "react-scroll"
-import { slide as Menu } from 'react-burger-menu'
+import "../styles/Navbar.css";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { slide as Menu } from "react-burger-menu";
 
-const Navbar = () => {    
-    const [menuOpen, setMenuOpen] = useState(false);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-
-    return (
-        <nav className="navbar">
-            <div className="navbar-content">
-                {/* <div className="navbar-circle-container">
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        {/* <div className="navbar-circle-container">
                     <div className="navbar-circle" id="navbar-circle-1">
                         <span></span>
                         <span></span>
@@ -18,83 +17,117 @@ const Navbar = () => {
                         <span></span>
                     </div>
                 </div> */}
-                <Link to="home-page" smooth={true}>
-                    <div className="navbar-logo">
-                        <span>H</span>
-                        <span>a</span>
-                        <span>c</span>
-                        <span>k</span>
-                        <span>C</span>
-                        <span>M</span>
-                        <span>U</span>
-                        <span> </span>
-                        <span>2</span>
-                        <span>0</span>
-                        <span>2</span>
-                        <span>3</span>
-                    </div>
-                    {/* <img className='navbar-logo' src="../../hackcmu-logo-black-short.png"></img>                 */}
-                </Link>
-                <div className="links-container">               
-                    <Link to="home-page" smooth={true}>
-                        <div className="link">Home</div>
-                    </Link>
-                    <Link to="schedule-page" smooth={true} offset={-0.08*window.innerHeight}>
-                        <div className="link">Schedule</div>
-                    </Link>
-                    <Link to="faq-page" smooth={true} offset={-0.06*window.innerHeight}>
-                        <div className="link">FAQ</div>
-                    </Link>
-                    <Link to="prizes-page" smooth={true} offset={-0.08*window.innerHeight}>
-                        <div className="link">Prizes</div>
-                    </Link>
-                    <Link to="sponsors-page" smooth={true} offset={-0.08*window.innerHeight}>
-                        <div className="link">Sponsors</div>
-                    </Link>
-                    <a
-                    id="mlh-trust-badge"
-                    style={{
-                        display: 'block',
-                        maxWidth: '100px',
-                        minWidth: '60px',
-                        position: 'fixed',
-                        right: '50px',
-                        top: '0',
-                        width: '10%',
-                        zIndex: 10000,
-                    }}
-                    href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
-                    target="_blank"
-                    >
-                    <img
-                        src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
-                        alt="Major League Hacking 2024 Hackathon Season"
-                        style={{ width: '100%' }}
-                    />
-                    </a>
+        <Link to="home-page" smooth={true}>
+          <div className="navbar-logo">
+            <span>H</span>
+            <span>a</span>
+            <span>c</span>
+            <span>k</span>
+            <span>C</span>
+            <span>M</span>
+            <span>U</span>
+            <span> </span>
+            <span>2</span>
+            <span>0</span>
+            <span>2</span>
+            <span>3</span>
+          </div>
+          {/* <img className='navbar-logo' src="../../hackcmu-logo-black-short.png"></img>                 */}
+        </Link>
+        <div className="links-container">
+          <Link to="home-page" smooth={true}>
+            <div className="link">Home</div>
+          </Link>
+          <Link
+            to="schedule-page"
+            smooth={true}
+            offset={-0.08 * window.innerHeight}
+          >
+            <div className="link">Schedule</div>
+          </Link>
+          <Link to="faq-page" smooth={true} offset={-0.06 * window.innerHeight}>
+            <div className="link">FAQ</div>
+          </Link>
+          <Link
+            to="prizes-page"
+            smooth={true}
+            offset={-0.08 * window.innerHeight}
+          >
+            <div className="link">Prizes</div>
+          </Link>
+          <Link
+            to="sponsors-page"
+            smooth={true}
+            offset={-0.08 * window.innerHeight}
+          >
+            <div className="link">Sponsors</div>
+          </Link>
+          <a
+            id="mlh-trust-badge"
+            style={{
+              display: "block",
+              maxWidth: "100px",
+              minWidth: "60px",
+              position: "fixed",
+              right: "50px",
+              top: "0",
+              width: "10%",
+              zIndex: 10000,
+            }}
+            href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
+            target="_blank"
+          >
+            <img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
+              alt="Major League Hacking 2024 Hackathon Season"
+              style={{ width: "100%" }}
+            />
+          </a>
+        </div>
+        <Menu
+          isOpen={menuOpen}
+          onStateChange={(state) => setMenuOpen(state.isOpen)}
+        >
+          <Link to="home-page" smooth={true}>
+            <div onClick={() => setMenuOpen(false)} className="link">
+              Home
             </div>
-            <Menu 
-                isOpen={menuOpen}
-                onStateChange={(state) => setMenuOpen(state.isOpen)}
-            >
-                <Link to="home-page" smooth={true}>
-                    <div onClick={() => setMenuOpen(false)} className="link">Home</div>
-                </Link>
-                <Link to="schedule-page" smooth={true} offset={-0.04*window.innerHeight}> 
-                    <div onClick={() => setMenuOpen(false)} className="link">Schedule</div>
-                </Link>
-                <Link to="faq-page" smooth={true} offset={-0.02*window.innerHeight}>
-                    <div onClick={() => setMenuOpen(false)} className="link">FAQ</div>
-                </Link>
-                <Link to="prizes-page" smooth={true} offset={-0.02*window.innerHeight}>
-                    <div onClick={() => setMenuOpen(false)} className="link">Prizes</div>
-                </Link>
-                <Link to="sponsors-page" smooth={true} offset={-0.04*window.innerHeight}>
-                    <div onClick={() => setMenuOpen(false)} className="link">Sponsors</div>
-                </Link>
-            </Menu>
+          </Link>
+          <Link
+            to="schedule-page"
+            smooth={true}
+            offset={-0.04 * window.innerHeight}
+          >
+            <div onClick={() => setMenuOpen(false)} className="link">
+              Schedule
             </div>
-        </nav>
-    );
-}
-export default Navbar
+          </Link>
+          <Link to="faq-page" smooth={true} offset={-0.02 * window.innerHeight}>
+            <div onClick={() => setMenuOpen(false)} className="link">
+              FAQ
+            </div>
+          </Link>
+          <Link
+            to="prizes-page"
+            smooth={true}
+            offset={-0.02 * window.innerHeight}
+          >
+            <div onClick={() => setMenuOpen(false)} className="link">
+              Prizes
+            </div>
+          </Link>
+          <Link
+            to="sponsors-page"
+            smooth={true}
+            offset={-0.04 * window.innerHeight}
+          >
+            <div onClick={() => setMenuOpen(false)} className="link">
+              Sponsors
+            </div>
+          </Link>
+        </Menu>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -1,26 +1,37 @@
 import "../styles/Prizes.css";
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
-import bluemech from '../images/prizes/blue-mech.jpg';
-import telescope from '../images/prizes/telescope.jpg';
-import starprojector from '../images/prizes/star-projector.jpg';
-import spikeball from '../images/prizes/spikeball-set.jpg';
-import skullcandy from '../images/prizes/skullcandy-earbuds.jpg';
-import projector from '../images/prizes/projector.jpg';
-import pinkblue from '../images/prizes/pink-blue.jpg';
-import navywhite from '../images/prizes/navywhite.jpg';
-import mouse from '../images/prizes/mouse.jpg';
-import ledlight from '../images/prizes/led-light.jpg';
-import isslegoset from '../images/prizes/iss-lego-set.jpeg';
-import echodot from '../images/prizes/echo-dot.jpg';
-import bobaplush from '../images/prizes/boba-plush.jpg';
+import bluemech from "../images/prizes/blue-mech.jpg";
+import telescope from "../images/prizes/telescope.jpg";
+import starprojector from "../images/prizes/star-projector.jpg";
+import spikeball from "../images/prizes/spikeball-set.jpg";
+import skullcandy from "../images/prizes/skullcandy-earbuds.jpg";
+import projector from "../images/prizes/projector.jpg";
+import pinkblue from "../images/prizes/pink-blue.jpg";
+import navywhite from "../images/prizes/navywhite.jpg";
+import mouse from "../images/prizes/mouse.jpg";
+import ledlight from "../images/prizes/led-light.jpg";
+import isslegoset from "../images/prizes/iss-lego-set.jpeg";
+import echodot from "../images/prizes/echo-dot.jpg";
+import bobaplush from "../images/prizes/boba-plush.jpg";
+
+const prizes = [
+  { image: telescope, title: "Telescope" },
+  { image: projector, title: "Projector" },
+  { image: skullcandy, title: "Skullcandy Earbuds" },
+  { image: bluemech, title: "Blue Mechanical Keyboard" },
+  { image: mouse, title: "Mouse" },
+  { image: echodot, title: "Echo Dot" },
+  { image: starprojector, title: "Star Projector" },
+  { image: ledlight, title: "LED Light" },
+  { image: spikeball, title: "Spikeball Set" },
+  { image: isslegoset, title: "ISS Lego Set" },
+  { image: pinkblue, title: "Pink and Blue Octopus Plush" },
+  { image: navywhite, title: "Navy and White Octopus Plush" },
+  { image: bobaplush, title: "Boba Plush" },
+];
 
 const Prizes = () => {
-  const data = [{"src": telescope, alt: "telescope"},
-{"src" : starprojector, alt: "star projector"}, 
-{"src" : spikeball, alt: "spike ball"},
-{"src" : skullcandy, alt: "sckull candy earbuds"}]
-  
   return (
     <div className="prizes-page">
       <div className="prizes-content">
@@ -44,61 +55,11 @@ const Prizes = () => {
           </div>
           <div className="prizes-caption-img-sep"></div>
           <Slide>
-            <div className="each-slide-effect">
-              <img  id="prize-image" className="prizes-img-and-caption" src={telescope} alt="telescope" />
-              <b> Telescope </b>
-            </div>
-            <div className="each-slide-effect">
-            <img  id="prize-image" className="prizes-img-and-caption" src={starprojector}  alt="star projector" />
-            <b> Star Projector </b>
-            </div>
-            <div className="each-slide-effect">
-            <img  id="prize-image" className="prizes-img-and-caption" src={spikeball}  alt="spikeball set" />
-            <b> Spikeball Set </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={skullcandy}  alt="skullcandy earbuds" />
-              <b> Skullcandy Earbuds </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src ={projector}  alt="projector" />
-              <b> Projector </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={pinkblue}  alt="pink and blue octopus plush" />
-              <b> Pink and Blue Octopus Plush </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={navywhite}  alt="navy and white octopus plush" />
-              <b> Navy and White Octopus Plush </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption"  src={mouse}  alt="mouse" />
-              <b> Mouse </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={ledlight}  alt="led light" />
-              <b> LED Light </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={isslegoset}  alt="iss lego set" />
-              <b> ISS Lego Set </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={echodot}  alt="echo dot" />
-              <b> Echo Dot </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={bobaplush}  alt="boba plush" />
-              <b> Boba Plush </b>
-            </div>
-            <div className="each-slide-effect">
-              <img id="prize-image" className="prizes-img-and-caption" src={bluemech}  alt="blue mechanical keyboard" />
-              <b> Blue Mechanical Keyboard </b>
-            </div>
+            {prizes.map((prize, index) => (
+              <PrizeItem key={index} image={prize.image} title={prize.title} />
+            ))}
           </Slide>
         </div>
-        <br></br>
         <br></br>
       </div>
     </div>
@@ -106,3 +67,19 @@ const Prizes = () => {
 };
 
 export default Prizes;
+
+const PrizeItem = ({ image, title }) => {
+  return (
+    <div className="each-slide-effect">
+      <div className="prize-title">{title}</div>
+      <div className="prize-img-container">
+        <img
+          id="prize-image"
+          className="prizes-img-and-caption"
+          src={image}
+          alt={title}
+        />
+      </div>
+    </div>
+  );
+};
